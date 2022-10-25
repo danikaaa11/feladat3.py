c872f67fbc089afd1d66ec7c6205a83a72068070
danikaaa11 <tihanyidaniel2005@gmail.com>
2022-10-25 11:17:37 +0200

    feladat5.1

--- feladat5.py
+++ feladat5.py
@@ -1,8 +1,9 @@
 import random
 
-lista = int(input("Adja meg hány elemű listát szeretne: "))
+#lista = int(input("Adja meg, hogy hány elemű listát szeretne: "))
+hossz = int(input("Adja meg a hosszt: "))
 
-veletlen=random.randint(-100,100)
-
-lista = [veletlen]
-print(lista)
+for i in range(hossz):
+    alista = [*range(-100, 100+1)]
+    veletlen = random.choice(alista)
+    print(veletlen)
