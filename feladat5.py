836d15c05120069e1ec85314c933bfc9e5cd21a8
danikaaa11 <tihanyidaniel2005@gmail.com>
2022-10-28 10:27:46 +0200

    feladat5.2

--- feladat5.py
+++ feladat5.py
@@ -1,9 +1,26 @@
 import random
 
 #lista = int(input("Adja meg, hogy hány elemű listát szeretne: "))
-hossz = int(input("Adja meg a hosszt: "))
+'''hossz = int(input("Adja meg a hosszt: "))
 
 for i in range(hossz):
     alista = [*range(-100, 100+1)]
     veletlen = random.choice(alista)
-    print(veletlen)
+    print(veletlen)'''
+    
+N=int(input("Hány elemmel  akarsz dolgozni: "))    
+ 
+Lszamok=[]    
+    
+for i in range(N): #N=3 0,1,2
+    Lszamok.append(random.randint(-100,100))
+    
+print(Lszamok)
+
+#a
+negativdb=0
+for szam in Lszamok:
+    if(szam<0):
+        negativdb+=1 #negativdb=negativdb+1
+
+print("Negatív számok száma: ",negativdb)           
